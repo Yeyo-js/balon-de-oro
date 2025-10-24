abeadd509be00052cdd72ba1c001b909d0b41255
Yeyo-js <craftyeyo6@gmail.com>
2025-10-24 15:10:23 -0500

    ADD: se creó el proyecto y se agragó el header y la lista de los jugadores ganadores desde el 2025 hasta el 2005, e imagenes de jugadores, club, pais.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <header className='header'>
+      <div className='div-img'> 
+        <img src="../public/imgs/logo-sin-fondo.png" alt=""  className='img'/>
+      </div>
+
+      <div className='div-title'>
+        <h1 className='title'>BALÓN DE ORO</h1>
+      </div>
+
+      <div className="div-search">
+        <FaSearch className="icon"/>
+        <input type="text" className="inp"/>
+      </div>
+    </header>
+    </>
+  )
+}
+
+export default App
